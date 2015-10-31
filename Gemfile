0d4e864519b1ee7f8c2d07019788ef0ed01a9d53
Estefania <sqr0502@gmail.com>
2015-10-31 15:59:49 -0400

    Add list of strains

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.2.4'
 group :development do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
+  gem 'httparty'
+  gem 'json'
 end
 
 group :production do
@@ -16,6 +18,7 @@ end
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/controllers/strains_controller.rb
+++ app/controllers/strains_controller.rb
@@ -1,4 +1,19 @@
 class StrainsController < ApplicationController
+
   def index
+    # url = "https://www.smokereports.com/api/v1.0/strains/search/day"
+    url = "http://leoqz.me:8443/api/strains"
+    response = HTTParty.get(url)
+    json_data = JSON.parse(response.body)
+    @strains = json_data["data"]
   end
+
+  # def parse_data(json_data
+  #   strains = []
+  #   # json_data['data'].each do |strain|
+  #   # return strain["name"]
+  #
+  #   end
+  # end
+
 end
