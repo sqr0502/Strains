b53859edcf3366b6bf02fa1833f2c7642e7cb727
Estefania <sqr0502@gmail.com>
2015-11-03 07:28:46 -0500

    css on profile page, removed qr code for now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102235521) do
+ActiveRecord::Schema.define(version: 20151103002542) do
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
